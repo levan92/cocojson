6df9d978aedc6057715ab0c49652c1ff44824a32
levan92 <lingevan0208@gmail.com>
2021-05-25 01:20:44 +0800

    update setup

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ setup(
     name='cocojson',
     version='0.1.0',
     description='Utility scripts for COCO json annotation format',
-    author='levan',
+    author='levan92',
     author_email='lingevan0208@gmail.com',
     url='https://github.com/levan92/cocojson',
+    license='MIT',
     packages=find_packages(exclude=('docs',))
 )
