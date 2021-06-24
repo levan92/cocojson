5aa310685b4051a75040e8ec16761445fd89b0b3
TeyrCrimson <teyr.crimson@gmail.com>
2021-06-24 14:23:44 +0800

    modding filter_cat to accommodate category_ids

--- cocojson/tools/filter_cat.py
+++ cocojson/tools/filter_cat.py
@@ -12,9 +12,23 @@ def filter_cat_from_files(coco_json, cats_to_keep,out_json=None):
     write_json_in_place(coco_json, out_dict, append_str='filtered',out_json=out_json)
 
 def filter_cat(coco_dict, cats_to_keep):
+    assert isinstance(cats_to_keep, list), cats_to_keep
+    try:
+        cats_to_keep = list(map(int, cats_to_keep))
+    except:
+        print("List doesn't contain ints, moving on.")
+    typecheck = type(cats_to_keep[0])
+    assert all(isinstance(cat, typecheck) for cat in cats_to_keep)
     if cats_to_keep and len(cats_to_keep):
         mapping_dict = {c:c for c in cats_to_keep}
     else:
         mapping_dict = {}
     new_cat_dict = coco_dict['categories']
-    return map_cat(coco_dict, new_cat_dict, mapping_dict, keep_old=False, map_is_id=False)
+    if typecheck == str:
+        print('string?')
+        return map_cat(coco_dict, new_cat_dict, mapping_dict, keep_old=False, map_is_id=False)
+    elif typecheck == int:
+        print('int?')
+        return map_cat(coco_dict, new_cat_dict, mapping_dict, keep_old=False, map_is_id=True)
+    else:
+        raise NotImplementedError
