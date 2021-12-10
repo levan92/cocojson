4d35f8236feee6805e4d2a27932305a640070ca2
levan92 <lingevan0208@gmail.com>
2021-12-10 12:31:31 +0800

    update version 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cocojson",
-    version="0.1.0",
+    version="0.1.1",
     author="levan92",
     author_email="lingevan0208@gmail.com",
     description="Utility functions for manipulating COCO json annotation format",
